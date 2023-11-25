630ddd4c0ef236d13803dd36ed33f2fbea46f0b1
jdbu2002 <jdbu2002@outlook.com>
2023-11-25 16:10:54 -0500

    feat: ✨ add map with pins

---
+++ javascript/map.js
@@ -0,0 +1,7 @@
+/**
+ * Función que abre el pop-up que muestra la información en el mapa.
+ * @param {string} pin - Nombre de pin que se oprimió
+ */
+function abrirMapa(pin) {
+  alert(`Se oprimió: ${pin}`);
+}
