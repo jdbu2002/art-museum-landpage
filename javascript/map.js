804143603e7d591c79a22117f12c57b808891af6
jdbu2002 <jdbu2002@outlook.com>
2023-11-25 17:25:10 -0500

    feat: ✨ add pop-ups for map content

--- javascript/map.js
+++ javascript/map.js
@@ -58,14 +58,116 @@ document.addEventListener("DOMContentLoaded", () => {
     // lo haya dejado de cliquear.
   }
 
-  // Le añado el evento para cuando se oprima el click en el mapa
+  // Le añado el evento para cuando se oprima el clic en el mapa
   map.addEventListener("mousedown", mouseDownHandler);
+
+  // Cierro el poup cuando se levante el mouse del mapa.
+  map.addEventListener("mouseup", closePinInfo);
 });
+
+let pinSelected = null;
 
 /**
  * Función que abre el pop-up que muestra la información en el mapa.
  * @param {string} pin - Nombre de pin que se oprimió.
  */
-function abrirMapa(pin) {
-  alert(`Se oprimió: ${pin}`);
+function openPinInfo(pin) {
+  const wasSame = closePinInfo();
+
+  if (wasSame) return;
+
+  const pinElement = document.getElementsByClassName(pin)[0];
+  const popUp = document.getElementById("mapPopUp").content.cloneNode(true);
+
+  popUp.getElementById("popUpTitle").textContent = mapContents[pin].title;
+  popUp.getElementById("popUpText").textContent = mapContents[pin].text;
+
+  if (mapContents[pin].className)
+    popUp.getElementById("mainPopUp").classList.add(mapContents[pin].className);
+
+  popUp.getElementById("mainPopUp").id = `${pin}PopUp`;
+  pinElement.appendChild(popUp);
+  pinSelected = pin;
 }
+
+/**
+ * Función que abre el pop-up que cierra la información en el mapa.
+ * @param {string} pin - Nombre de pin que se oprimió.
+ * @returns {boolean} Si el pin que se oprimió es el mismo que el que está
+ *                    abierto.
+ */
+function closePinInfo(pin) {
+  const wasSame = pinSelected === pin;
+
+  if (pinSelected) {
+    const pinElement = document.getElementById(`${pinSelected}PopUp`);
+    pinElement.remove();
+    pinSelected = null;
+  }
+
+  return wasSame;
+}
+
+const mapContents = {
+  pasillo: {
+    title: "Gran pasillo",
+    text: "La entrada del museo, donde hay exhibiciones temporales.",
+  },
+  romano: {
+    title: "Arte griego y romano",
+    text: "Encuentra obras del periodo neolítico hasta el romano en el 312 A.C.",
+  },
+  africa: {
+    title: "África, oceanía y américas",
+    text: "Conoce la colección del Sahara africano, las islas pacíficas y norte, centro y sur américa.",
+  },
+  moderno: {
+    title: "Arte moderno y contemporaneo",
+    text: "Visualiza la obras modernas desde los años 1890 hasta el presente de la mano del departamento de arte moderno.",
+  },
+  robert: {
+    title: "Colección de Robert Lehman",
+    text: "La Colección Robert Lehman es una de las colecciones de arte reunidas de forma privada más distinguidas de los Estados Unidos.",
+  },
+  medieval: {
+    title: "Arte medieval",
+    text: "La colección de arte medieval y bizantino del museo se encuentra entre las más completas del mundo.",
+  },
+  america: {
+    title: "Ala americana",
+    text: 'En 1924 se inauguró un edificio separado "American Wing" para exhibir las artes domésticas del siglo XVII y principios del XIX.',
+  },
+  armaduras: {
+    title: "Armas y armaduras",
+    text: "Las armas y armaduras han sido una parte vital de prácticamente todas las culturas durante miles de años, fundamentales no sólo en la conquista y la defensa, sino también en el boato de la corte y los eventos ceremoniales.",
+  },
+  egipcios: {
+    title: "Arte egipcio",
+    text: "La colección Met de arte del antiguo Egipto consta de aproximadamente 26.000 objetos de importancia artística, histórica y cultural, que datan del Paleolítico al período romano.",
+  },
+  tienda: {
+    title: "La Tienda MET",
+    text: "Obsequios de bellas artes reproducidos de colecciones, libros, videos, impresiones de edición limitada y joyas.",
+  },
+  biblioteca: {
+    title: "Biblioteca Thomas J. Watson",
+    text: "La Biblioteca Thomas J. Watson es la biblioteca de investigación central del Museo Metropolitano de Arte. Su colección de libros y publicaciones periódicas relacionadas con la historia del arte es una de las más completas del mundo.",
+  },
+  galeria: {
+    title: "Galeria 199",
+    text: "Aquí hay exposiciones variadas y por tiempo limitado. ¡Visítala para ver que nuevo artista está exponiendo!",
+  },
+  cafe: {
+    title: "Café Petrie Court",
+    text: "Abierto los viernes y sábados por la noche para la cita nocturna, este café, ubicado en una espectacular galería de esculturas, ofrece bebidas y comidas ligeras, además de música en vivo.",
+  },
+  auditorio: {
+    title: "Auditorio Grace Rainey Rogers",
+    text: "Inaugurado en 1954 y actualizado con proyección de video, iluminación inteligente y audio digital de última generación, el Auditorio Grace Rainey Rogers del Museo alberga reuniones dinámicas, conferencias, proyecciones de películas, espectáculos musicales y de danza, y conciertos de ópera.",
+  },
+  templo: {
+    title: "Templo de Dendur",
+    text: "Aunque pequeño en comparación con la mayoría de los templos egipcios, el Templo de Dendur es un elegante ejemplo de un templo faraónico típico y es lo más destacado de cualquier visita al Met.",
+    className: "templo",
+  },
+};
